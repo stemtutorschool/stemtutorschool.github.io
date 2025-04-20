019c9d58d8cf6a770e4ef1614314b1e207f50315
stemtutorschool <stemtutorschool@gmail.com>
2025-04-20 17:47:48 -0400

    Add files via upload

---
+++ videoData.js
@@ -0,0 +1,13 @@
+// videoData.js - Sample data for STEM School videos
+const sampleVideos = [
+    {
+        id: 1,
+        title: "Subtract within 20 by using blocks | Math | Pre-algebra | Grade 2 - 4",
+        grade: "2-4",
+        topic: "pre-algebra",
+        thumbnail: "https://www.youtube.com/watch?v=Qw6VkK7PKZ4",
+        uploadDate: "2025-03-15",
+        youtubeId: "Qw6VkK7PKZ4"
+    }
+    // You can add more video entries here as needed
+];
