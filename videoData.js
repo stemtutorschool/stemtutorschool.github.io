45732d74c839a6b891d9e5316874b12a6ac16f12
stemtutorschool <stemtutorschool@gmail.com>
2025-04-21 14:24:35 -0400

    Add files via upload

--- videoData.js
+++ videoData.js
@@ -8,6 +8,42 @@ const sampleVideos = [
         thumbnail: "https://www.youtube.com/watch?v=Qw6VkK7PKZ4",
         uploadDate: "2025-03-15",
         youtubeId: "Qw6VkK7PKZ4"
+    },
+    {
+        id: 2,
+        title: "Identifying value in digits (3 digits) | Math | Pre-algebra | Grade 2 - 4",
+        grade: "2-4",
+        topic: "pre-algebra",
+        thumbnail: "https://www.youtube.com/watch?v=xzQXt8JcRZo",
+        uploadDate: "2025-03-15",
+        youtubeId: "xzQXt8JcRZo"
+    },
+    {
+        id: 3,
+        title: "Place Value Table within 1000 | Math | Pre-algebra | Grade 2 - 4",
+        grade: "2-4",
+        topic: "pre-algebra",
+        thumbnail: "https://www.youtube.com/watch?v=HXjkrWWSrc4",
+        uploadDate: "2025-03-15",
+        youtubeId: "HXjkrWWSrc4"
+    },
+    {
+        id: 4,
+        title: "Add - within 20 by using number line  | Math | Pre-algebra | Grade 2 - 4",
+        grade: "2-4",
+        topic: "pre-algebra",
+        thumbnail: "https://www.youtube.com/watch?v=jDVkCPcn9Pk",
+        uploadDate: "2025-03-15",
+        youtubeId: "jDVkCPcn9Pk"
+    },
+    {
+        id: 5,
+        title: "Add - within 20 by using 10 frames  | Math | Pre-algebra | Grade 2 - 4",
+        grade: "2-4",
+        topic: "pre-algebra",
+        thumbnail: "https://www.youtube.com/watch?v=wR2S4ylKJKQ",
+        uploadDate: "2025-03-15",
+        youtubeId: "wR2S4ylKJKQ"
     }
     // You can add more video entries here as needed
 ];
